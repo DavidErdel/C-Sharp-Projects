07273ffba4d3a92b94a6729f6c2f12344b3c943c
DavidErdel <daviderdel@gmail.com>
2022-07-28 14:58:53 -0500

    Exceptions and Handling

---
+++ Basic_C#_Programs/ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            List<int> numbers = new List<int>() { 2, 4, 8, 16, 24 };
+            Console.WriteLine("Please input a whole number");
+            int userAnswer = Convert.ToInt32(Console.ReadLine());
+
+
+            foreach (int n in numbers)
+            {
+                Console.WriteLine(n / userAnswer);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        Console.WriteLine("The program has emerged from the try/catch block and continued on with program execution");
+        Console.ReadLine();
+    }
+}
+
