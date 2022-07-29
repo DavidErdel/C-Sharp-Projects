11715e76252a6e7e404139ed504f0540966fd7d5
DavidErdel <daviderdel@gmail.com>
2022-07-28 20:17:01 -0500

    Calling Methods

---
+++ Basic_C#_Programs/CallingMethodsAssignment/CallingMethodsAssignment/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    public class Class1 //Creates a public class called "Class1"//
+    {
+        public int add(int input ) //Declares the first method, called "add"//
+        {
+            int result = input + 1; //Defines an integer called "result" as the input plus 1//
+            return result; //Returns result//
+        }
+        public int sub(int input) //Declares the first method, called "sub"//
+        {
+            int result = input - 1; //Defines an integer called "result" as the input minus 1//
+            return result; //Returns result//
+        }
+        public int mult(int input) //Declares the first method, called "mult"//
+        {
+            int result = input * 2; //Defines an integer called "result" as the input times 2//
+            return result; //Returns result//
+        }
+    }
+}
---
+++ Basic_C#_Programs/CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 one = new Class1();
+
+            Console.WriteLine("What number do you want to do math operations on?"); //Asks the user which number they want to do math operations on//
+            int userAnswer = Convert.ToInt32(Console.ReadLine()); //Converts the user answer from a string to an integer//
+
+            int result = one.add(userAnswer); //Calls the add method and applies it to userAnswer//
+            Console.WriteLine(result); //Writes the result of calling the add method and applying it to userAnswer//
+
+            result = one.sub(userAnswer); //Calls the sub method and applies it to userAnswer//
+            Console.WriteLine(result); //Writes the result of calling the sub method and applying it to userAnswer//
+
+            result = one.mult(userAnswer); //Calls the mult method and applies it to userAnswer//
+            Console.WriteLine(result); //Writes the result of calling the mult method and applying it to userAnswer//
+
+
+            Console.ReadLine();
+        }
+    }
+}
