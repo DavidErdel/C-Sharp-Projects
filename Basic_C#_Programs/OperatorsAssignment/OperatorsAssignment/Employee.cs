b38645e2b6bf9ef87efbb41d79625ad6c83ac39f
DavidErdel <daviderdel@gmail.com>
2022-08-02 15:11:35 -0500

    Operator overload

---
+++ Basic_C#_Programs/OperatorsAssignment/OperatorsAssignment/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OperatorsAssignment
+{
+    public class Employee
+    {
+        public int Id; //Creates a property called id of the data type int//
+        public string FirstName; //Creates a property called FirstName of the data type string//
+        public string LastName; //Creates a property called LastName of the data type string//
+
+        public static bool operator== (Employee one, Employee two) //Overloads the == operator//
+        {
+            return one.Id == two.Id; //Returns true if one.Id == two.Id and false otherwise//
+        }
+
+        public static bool operator!= (Employee one, Employee two) //Overloads the != operator//
+        {
+            return one.Id != two.Id; //Returns false is one.Id == two.Id and false otherwise//
+        }
+
+    }
+}
---
+++ Basic_C#_Programs/OperatorsAssignment/OperatorsAssignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OperatorsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee one = new Employee(); //Creates a new object from the Employee class called one//
+            Employee two = new Employee(); //Creates a new object from the Employee class called two//
+
+            one.Id = 1; //Assigns the value of one's Id to 1//
+            two.Id = 2; //Assigns the value of two's Id to 2//
+
+            Console.WriteLine(one == two); //Prints the value of one == two to the console, which in this case will be false//
+        }
+    }
+}
