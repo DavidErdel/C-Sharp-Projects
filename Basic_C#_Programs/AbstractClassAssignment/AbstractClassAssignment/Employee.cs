c701280f6a375d8e972dc262f8a43f24e9cd54dd
DavidErdel <daviderdel@gmail.com>
2022-08-01 22:05:13 -0500

    Abstract class assignment

---
+++ Basic_C#_Programs/AbstractClassAssignment/AbstractClassAssignment/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractClassAssignment
+{
+    public class Employee : Person //Creates a class called Employee that inherits from Person//
+    {
+        public void SayName() { Console.WriteLine("Name: " + FirstName + LastName); } //Implements the SayName method inside of the Employee class//
+
+    }
+}
---
+++ Basic_C#_Programs/AbstractClassAssignment/AbstractClassAssignment/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractClassAssignment
+{
+    public abstract class Person //Creates an abstract class called Persons//
+    {
+
+        public string FirstName { get; set; } //Creates a string property called FirstName//
+
+        public string LastName { get; set; } //Creates a string property called LastName//
+
+        public void SayName() //Creates the method SayName//
+        {
+        }
+
+
+    }
+}
