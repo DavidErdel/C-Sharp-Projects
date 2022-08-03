14b2b8628665bddf463c50318b11a34cafd5da46
DavidErdel <daviderdel@gmail.com>
2022-08-02 23:32:24 -0500

    Struct

---
+++ Basic_C#_Programs/StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num = new Number(); //Creates an object of the data type Number called num//
+            num.Amount = 2; //Assigns the amount of num to 2//
+
+            Console.WriteLine(num.Amount); //Prints the amount of num
+            Console.ReadLine();
+        }
+        public struct Number //Creates a struct called Number//
+        {
+            public decimal Amount; //Gives Number the decimal property Amount//
+        }
+    }
+}
