19f7bc0119299dddec7ae59245c5e7b54906f63d
DavidErdel <daviderdel@gmail.com>
2022-08-05 12:08:35 -0500

    Constructor

---
+++ Basic_C#_Programs/ConstructorAssignment/ConstructorAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConstructorAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            const string name = "David"; //Creates a const variable//
+
+            var age = 37; //Creates a variable using the keyword "var"//
+
+        }
+        public class Person //Creates a public class called Person//
+        {
+            public string fname; //Creates a public string called fname//
+            public string lname; //Creates a public string called lname//
+            
+            public Person(string fname) : this (fname, "Smith") //Creates a contructor that is chained to the second constructor//
+            {
+
+            }
+            public Person(string fname, string lname) //Creates a second constructor//
+            {
+                this.fname = fname;
+                this.lname = lname;
+            }
+        }
+    }
+}
