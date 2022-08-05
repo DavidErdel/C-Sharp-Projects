cba29321319758a9f5b563f74f86635721e3607c
DavidErdel <daviderdel@gmail.com>
2022-08-05 08:07:53 -0500

    Input

---
+++ Basic_C#_Programs/InputAssignment/InputAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace InputAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            int userAnswer = Convert.ToInt32(Console.ReadLine());
+
+            using (StreamWriter file = new StreamWriter(@"C:\Users\David\Documents\log2.txt", true))
+            {
+                file.WriteLine(userAnswer);
+                file.Close();
+                string file2 = File.ReadAllText(@"C:\Users\David\Documents\log2.txt");
+                Console.WriteLine("The number you entered is " + file2);
+            }
+            Console.ReadLine();
+        }
+    }
+}
